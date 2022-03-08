6a5bb9e06619f265f3141a5773a2c6acd90e62fc
Ali-Aziz-nti-johanneberg <ali.aziz@elev.ga.ntig.se>
2022-03-08 15:31:17 +0100

    Improvmernt

--- slutprojekt/app.rb
+++ slutprojekt/app.rb
@@ -14,24 +14,31 @@ end
 get('/') do
     db = connect_to_db("db/db.db")
     recipe_name = db.execute("SELECT * FROM recipe")
-
     p "re är: #{recipe_name}"
     slim(:index,locals:{rec:recipe_name})
 end
 
+get('/recipes/new') do
+    slim(:new)
+end
+
+post('/recipes/new') do
+    recipe_name = params[:name_recipe]
+    ingrediens_list = params[:name_ingrediens]
+    p "#{ingrediens_list} and #{recipe_name}"
+    db = SQLite3::Database.new("db/db.db")
+    db.execute('INSERT INTO recipe (recipe_name) VALUE (?)',recipe_name)
+    redirect('/')
+end
+
+
 get('/recipes/:id') do
     id = params[:id]
     db = connect_to_db("db/db.db")
-    recipe_show = db.execute("SELECT * FROM recipe WHERE id=?",id)
+    recipe_show = db.execute("SELECT * FROM recipe WHERE id=?",id)[0]
     p "re är: #{recipe_show}"
     slim(:show,locals:{info:recipe_show})
 end
-
-get('/recipes/new') do
-    slim(:show)
-end
-
-
 
 get('/login') do
     slim(:login)
