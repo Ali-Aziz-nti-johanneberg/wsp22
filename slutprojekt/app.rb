d4c74c967f5b1591da32c7121c7afc072dafc83b
Ali-Aziz-nti-johanneberg <ali.aziz@elev.ga.ntig.se>
2022-04-05 15:33:14 +0200

    Module done

--- slutprojekt/app.rb
+++ slutprojekt/app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+require_relative './model.rb'
 
 enable :sessions
 
@@ -26,8 +27,7 @@ end
 post('/login') do
     username = params[:username]
     password = params[:password]
-    db = connect_to_db("db/db.db")
-    result = db.execute('SELECT * FROM user WHERE username = ?',username).first
+    user_info(username)
     pwdigest = result["pwdigest"]
     id = result["id"]
     if BCrypt::Password.new(pwdigest) == password && username.empty? == false
@@ -47,55 +47,43 @@ post('/recipes') do
     recipe_name = params[:name_recipe]
     ingrediens_list = params[:name_ingrediens]
     p "#{ingrediens_list} and #{recipe_name}"
-    db = SQLite3::Database.new("db/db.db")
-    db.execute('INSERT INTO recipe (recipe_name,user_id) VALUES (?,?)',recipe_name,id)
+    add_into_recipie(recipe_name,id)
     ingre_array = ingrediens_list.split(",")
-    ingrediens_database = db.execute('SELECT ingredients_name FROM ingredients')
+    ingrediens_database = ingrediens_database_show()
     i = 0
     while i < ingre_array.length
         if ingrediens_database.flatten.include?(ingre_array[i].to_s) == false
-            db.execute('INSERT INTO ingredients (ingredients_name) VALUES (?)',ingre_array[i].to_s)
+            add_into_ingredients(ingre_array,i)
         end
-        id_ing_temp = db.execute('SELECT id FROM ingredients WHERE ingredients_name = ?',ingre_array[i].to_s)
-        p id_ing_temp
-        id_rec_temp = db.execute('SELECT id FROM recipe WHERE id = (SELECT MAX(ID)  FROM recipe);')
-        p id_rec_temp
-        db.execute('INSERT INTO ingredients_recipes (ingredient_id, recipe_id) VALUES (?,?)',id_ing_temp, id_rec_temp)
+        insert_id_into_many(i,ingre_array)
         i += 1
     end
-
     redirect('/')
 end
 
 
 get('/recipes/:id') do
     id = params[:id]
-    db = connect_to_db("db/db.db")
-    db.results_as_hash = true
-    show = db.execute("SELECT ingredients_name FROM ingredients WHERE id IN (SELECT ingredient_id FROM ingredients_recipes WHERE recipe_id = ?)",id)
+    show = show_one_recipe(id)
     slim(:show,locals:{info:show})
 end
 
 post('/recipes/:id/delete') do
   id = params[:id].to_i
-  db = SQLite3::Database.new("db/db.db")
-  db.execute("DELETE FROM recipe WHERE id = ?",id)
+  delete_one_recipe(id)
   redirect('/')
 end
 
 get('/recipes/:id/edit') do
   id = params[:id].to_i
-  db = SQLite3::Database.new("db/db.db")
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM recipe WHERE Id = ?",id).first
+  result = edit_recipe(id)
   slim(:"edit",locals:{result:result})
 end
 
 post('/recipes/:id/update') do
   id = params[:id].to_i
   name = params[:name]
-  db = SQLite3::Database.new("db/db.db")
-  db.execute("UPDATE recipe SET recipe_name = ? WHERE Id = ? ",name,id)
+  update_recipe(id,name)
   redirect('/')
 end
 
@@ -107,12 +95,11 @@ post('/users/new') do
     username = params[:username]
     password = params[:password]
     password_confirm = params[:password_confirm]
-    db = connect_to_db("db/db.db")
-    result = db.execute("SELECT username FROM user WHERE username=?",username)
+    result = select_username(username)
     if result.empty?
         if (password == password_confirm)
             password_digest = BCrypt::Password.create(password)
-            db.execute('INSERT INTO user (username,pwdigest) VALUES (?,?)',username,password_digest)
+            create_user(username,password_digest)
             redirect('/showlogin')
         else
             "Password do not match"
@@ -127,11 +114,10 @@ end
 get('/user') do
     db = connect_to_db("db/db.db")
     id = session[:id].to_i
-    admin = db.execute("SELECT username FROM user WHERE id = ?",id)
     if id == 1
-        recipe_name = db.execute("SELECT * FROM recipe")
+        recipe_name = all_recipe()
     else
-        recipe_name = db.execute("SELECT * FROM recipe WHERE user_id = ?",id)
+        recipe_name = all_recipe_by(id)
     end
     p "re är: #{recipe_name}"
     slim(:edit_user_index,locals:{rec:recipe_name})
--- slutprojekt/model.rb
+++ slutprojekt/model.rb
@@ -3,3 +3,80 @@ def connect_to_db(path)
     db.results_as_hash = true
     return db
 end   
+
+def user_info(username)
+    db = connect_to_db("db/db.db")
+    result = db.execute('SELECT * FROM user WHERE username = ?',username).first
+    return result
+end
+
+def add_into_recipie(recipe_name,id)
+    db = SQLite3::Database.new("db/db.db")
+    db.execute('INSERT INTO recipe (recipe_name,user_id) VALUES (?,?)',recipe_name,id)
+end
+
+def ingrediens_database_show()
+    db = SQLite3::Database.new("db/db.db")
+    ingrediens_database = db.execute('SELECT ingredients_name FROM ingredients')
+    return ingrediens_database
+end
+
+def add_into_ingredients(ingre_array,i)
+    db = SQLite3::Database.new("db/db.db")
+    db.execute('INSERT INTO ingredients (ingredients_name) VALUES (?)',ingre_array[i].to_s)
+end
+
+def insert_id_into_many(i,ingre_array)
+    db = SQLite3::Database.new("db/db.db")
+    id_ing_temp = db.execute('SELECT id FROM ingredients WHERE ingredients_name = ?',ingre_array[i].to_s)
+    #Select the id of every ingredient in the array from the tabel.
+    id_rec_temp = db.execute('SELECT id FROM recipe WHERE id = (SELECT MAX(ID)  FROM recipe);')
+    #Select id from the latest tabell recipe
+    db.execute('INSERT INTO ingredients_recipes (ingredient_id, recipe_id) VALUES (?,?)',id_ing_temp, id_rec_temp)
+    #Insert ingredient id and recipie id into relation tabel
+end
+
+def show_one_recipe(id)
+    db = connect_to_db("db/db.db")
+    return db.execute("SELECT ingredients_name FROM ingredients WHERE id IN (SELECT ingredient_id FROM ingredients_recipes WHERE recipe_id = ?)",id)
+end
+
+def delete_one_recipe(id)
+    db = SQLite3::Database.new("db/db.db")
+    db.execute("DELETE FROM recipe WHERE id = ?",id)
+end
+
+def edit_recipe(id)
+    db = SQLite3::Database.new("db/db.db")
+    result = db.execute("SELECT * FROM recipe WHERE Id = ?",id).first
+    return result
+end
+
+def update_recipe(id,name)
+    db = SQLite3::Database.new("db/db.db")
+    db.execute("UPDATE recipe SET recipe_name = ? WHERE Id = ? ",name,id)
+end
+
+def select_username(username)
+    db = connect_to_db("db/db.db")
+    result = db.execute("SELECT username FROM user WHERE username=?",username)
+    return result
+end
+
+def create_user(username,password_digest)
+    db = connect_to_db("db/db.db")
+    db.execute('INSERT INTO user (username,pwdigest) VALUES (?,?)',username,password_digest)
+end
+
+def all_recipe()
+    db = connect_to_db("db/db.db")
+    result = db.execute("SELECT * FROM recipe")
+    return result
+end
+
+def all_recipe_by(id)
+    db = connect_to_db("db/db.db")
+    result = db.execute("SELECT * FROM recipe WHERE user_id = ?",id)
+    return result
+end
+
